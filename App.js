8c104b6058f2594898aa5bb0c72a36bd49c43f9b
liahoney <toridoc@gmail.com>
2023-03-15 18:16:40 +0900

    modify

--- App.js
+++ App.js
@@ -3,13 +3,63 @@ import { SectionList } from "react-native";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import BusInfo from "./src/BusInfo";
 import { COLOR } from "./src/color";
-import { busStop, getSections, getBusNumColorBytype } from "./src/data";
+import { busStop, getSections, getBusNumColorBytype, getRemainedTimeText, getSeatStatusText, getBusNumColorByType } from "./src/data";
+import dayjs  from 'dayjs';
+import { useEffect, useState } from "react";
 
 export default function App() {
   const sections = getSections(busStop.buses);
-
+  const [now, setNow ] = useState(dayjs())
+  
+  
   const renderItem = ({ item: bus }) => {
-    const numColor = getBusNumColorBytype(bus.type);
+  
+   
+    const numColor = getBusNumColorByType(bus.type);
+   
+       /**
+     * Startr
+     */
+    // undefined ?? null -> null 
+    // { ... } ?? null -> { ... }
+    const firstNextBusInfo = bus.nextBusInfos?.[0] ?? null; 
+    const secondNextBusInfo = bus.nextBusInfos?.[1] ?? null;
+    const newNextBusInfos =
+      !firstNextBusInfo && !secondNextBusInfo
+        ? [null]
+        : [firstNextBusInfo, secondNextBusInfo];
+
+        if(bus.num === 2000) {
+          console.log('bus.num === 146', newNextBusInfos)
+        }
+    
+    // if (bus.num === 2000) {
+    //   console.log(bus.num, 'newNextBusInfos', newNextBusInfos); // TODO: 확인
+    // }
+
+    const processedNextBusInfos = newNextBusInfos.map((info) => {
+      if (!info)
+        return {
+          hasInfo: false,
+          remainedTimeText: "도착 정보 없음",
+        };
+
+      const { arrivalTime, numOfRemainedStops, numOfPassengers } = info;
+      const remainedTimeText = getRemainedTimeText(now, arrivalTime);
+      const seatStatusText = getSeatStatusText(bus.type, numOfPassengers);
+      return {
+        hasInfo: true,
+        remainedTimeText,
+        numOfRemainedStops,
+        seatStatusText,
+      };
+    });
+    /**
+     * End
+     */
+
+
+  
     return (
       <BusInfo
         isBookmarked={bus.isBookmarked}
@@ -17,10 +67,21 @@ export default function App() {
         num={bus.num}
         directionDescription={bus.directionDescription}
         numColor={numColor}
+        processedNextBusInfos={processedNextBusInfos}
       />
     );
   };
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newNow = dayjs()
+    setNow(newNow)
+    },1000)
 
+    return () => {
+      clearInterval(interval)
+    }
+  },[])
   return (
     <SafeAreaView style={styles.container}>
       <SectionList
--- src/BusInfo.js
+++ src/BusInfo.js
@@ -10,10 +10,11 @@ export default ({
     onPressBookmark,
     num,
     numColor,
-    directionDescription
+    directionDescription,
+    processedNextBusInfos
 }) => {
   return (
-    <View style={{ flexDirection: "row", backgroundColor: 'pink', }}>
+    <View style={{ flexDirection: "row"}}>
       <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
         {/* 북마크 */}
         <BookmarkButton 
@@ -30,7 +31,16 @@ export default ({
       <View style={{ flex:1 ,flexDirection: "row", alignItems: "center" }}>
         {/* M분 S초 /N번째 전/ 여유 */}
         <View style={{ flex: 1}}>
-       <NextBusInfo
+          {processedNextBusInfos.map((info, index) => (
+             <NextBusInfo
+             key={`next-bus-info-${index}`}
+             hasInfo={info.hasInfo}
+             remainedtimeText={info.remainedTimeText}
+             numOfRemainedStops={info.numOfRemainedStops}
+             seatStatusText={info.seatStatusText}
+             />
+          ))}
+       {/* <NextBusInfo
          hasInfo={true}
          remainedtimeText={"8분 0초"}
          numOfRemainedStops={5}
@@ -39,7 +49,7 @@ export default ({
         <NextBusInfo 
           hasInfo={false}
           remainedtimeText="도착 정보 없음"
-        />
+        /> */}
         </View>
         {/* 알람아이콘 */}
         <AlarmButton onPress={() => {}} style={{paddingHorizontal: 15 }}/>
